372eab5ee5520c2f55f38917acdd9e8b0d6eb634
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-11-13 22:46:39 +0500

    created routes for logging in and out, added error handlers for server and response

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,11 @@ const crypto = require("crypto");
 const cloudinary = require("cloudinary");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
-exports.registerUser = async (req, res, next) => {
-  const { name, email, password, avatar } = req.body;
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password, avatar, displayName } = req.body;
 
   const user = await User.create({
     name,
@@ -14,7 +16,45 @@ exports.registerUser = async (req, res, next) => {
       public_id: avatar.public_id,
       url: avatar.url,
     },
+    displayName,
   });
 
   sendToken(user, 201, res);
-};
+});
+
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Checks if email and password is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
+
+  // Finding user in database
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  // Checks if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  sendToken(user, 200, res);
+});
+
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged out",
+  });
+});
--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,13 @@ app.use("/api/v1", user);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
